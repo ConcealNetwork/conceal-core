0472ffae53287a03731b76da56e399de51f34d62
cryptokatz <kahthan@gmail.com>
2018-08-20 19:29:16 +0530

    add chaos

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -147,9 +147,10 @@ const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "f7061e9a5f0d3054
 
 // Seed Nodes
 const std::initializer_list<const char*> SEED_NODES = {
-		"212.237.59.97:15000",
-		"94.177.171.102:15000",
-		"188.213.165.210:15000"
+		"212.237.59.97:15000", // gamma
+		"94.177.171.102:15000", // aurora
+		"188.213.165.210:15000", // omega
+		"93.186.254.77:15000" // chaos
 };
 
 struct CheckpointData {
