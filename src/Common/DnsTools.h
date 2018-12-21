85b250881c3b1666eaa7b3b7de9038c628b1a47e
darkharvest <kahthan@gmail.com>
2018-12-21 11:07:18 +0530

    added load checkpoints from file and dns

---
+++ src/Common/DnsTools.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2017-2018, Karbo developers
+//
+// This file is part of Karbo.
+//
+// Karbo is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Karbo is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with Karbo. If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace Common {
+
+#ifndef __ANDROID__
+
+  bool fetch_dns_txt(const std::string domain, std::vector<std::string>&records);
+
+#endif
+
+}
