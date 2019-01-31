008772f6b88adda6e27e68bc935b4246e6896a73
cryptokatz <kahthan@gmail.com>
2019-01-31 10:22:48 +0530

    Update SimpleWallet.cpp

--- src/SimpleWallet/SimpleWallet.cpp
+++ src/SimpleWallet/SimpleWallet.cpp
@@ -1436,7 +1436,7 @@ bool simple_wallet::listTransfers(const std::vector<std::string>& args) {
   bool haveTransfers = false;
   bool haveBlockHeight = false;
   std::string blockHeightString = ""; 
-  uint blockHeight = 0;
+  uint32_t blockHeight = 0;
   WalletLegacyTransaction txInfo;
 
 
