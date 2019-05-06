138752fbcad4ed07fd7a9f762067ea7f01d1d4de
darkharvest <kahthan@gmail.com>
2019-05-06 11:28:00 +0530

    add confirmations to serialization

--- src/PaymentGate/PaymentServiceJsonRpcMessages.h
+++ src/PaymentGate/PaymentServiceJsonRpcMessages.h
@@ -197,7 +197,7 @@ struct TransactionRpcInfo {
   std::string transactionHash;
   uint32_t blockIndex;
   uint64_t timestamp;
-  uint32_t confirmations = 0;
+  uint32_t confirmations;
   bool isBase;
   uint64_t unlockTime;
   int64_t amount;
