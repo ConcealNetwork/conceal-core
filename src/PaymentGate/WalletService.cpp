6b28ce625ace730c2ea69fce9655ec7ff7fe3965
katz <cryptokatz1@protonmail.com>
2019-12-04 12:37:58 +0530

    Update WalletService.cpp

--- src/PaymentGate/WalletService.cpp
+++ src/PaymentGate/WalletService.cpp
@@ -989,13 +989,10 @@ std::error_code WalletService::sendTransaction(const SendTransaction::Request& r
       sendParams.extra = Common::asString(Common::fromHex(request.extra));
     }
 
-    sendParams.fee = CryptoNote::parameters::MINIMUM_FEE_V2;
-
-
     sendParams.sourceAddresses = request.sourceAddresses;
     sendParams.destinations = convertWalletRpcOrdersToWalletOrders(request.transfers);
     sendParams.messages = convertWalletRpcMessagesToWalletMessages(messages);
-    sendParams.fee = request.fee;
+    sendParams.fee = 1000;
     sendParams.mixIn = request.anonymity;
     sendParams.unlockTimestamp = request.unlockTime;
     sendParams.changeDestination = request.changeAddress;
