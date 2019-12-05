1f4ab9c931ee5a7b8612c4ab13bcda9bea65eb36
katz <cryptokatz1@protonmail.com>
2019-12-05 14:04:59 +0530

    Set upgrade height

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -67,7 +67,7 @@ const uint32_t DEPOSIT_MAX_TERM = 1 * 12 * 21900; /* legacy deposts - one year *
 const uint32_t DEPOSIT_MAX_TERM_V1 = 64800 * 20; /* five years */
 const uint32_t DEPOSIT_MIN_TERM_V3 = 21900; /* consensus 2019 - one month */
 const uint32_t DEPOSIT_MAX_TERM_V3 = 1 * 12 * 21900; /* consensus 2019 - one year */
-const uint32_t DEPOSIT_HEIGHT_V3 = 308687; /* consensus 2019 - effective height */
+const uint32_t DEPOSIT_HEIGHT_V3 = 413400; /* consensus 2019 - deposts v3.0 */
 const uint64_t DEPOSIT_MIN_TOTAL_RATE_FACTOR = 0; /* constant rate */
 const uint64_t DEPOSIT_MAX_TOTAL_RATE = 4; /* legacy deposits */
 
