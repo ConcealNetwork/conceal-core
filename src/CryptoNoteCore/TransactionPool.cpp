9568875e4d36e7a678885e82d829bb3dec504649
katz <cryptokatz1@protonmail.com>
2019-12-05 11:48:52 +0530

    Update TransactionPool.cpp

--- src/CryptoNoteCore/TransactionPool.cpp
+++ src/CryptoNoteCore/TransactionPool.cpp
@@ -388,13 +388,9 @@ namespace CryptoNote {
 
     BlockTemplate blockTemplate;
 
-  if (height > CryptoNote::parameters::DEPOSIT_HEIGHT_V3) 
-  {
-
-  /* Process the latest deposit transactions first */
     for (auto it = m_fee_index.rbegin(); it != m_fee_index.rend() && it->fee == 1000; ++it) 
     {
-      const auto& txd = *i;
+      const auto& txd = *it;
 
       if (m_ttlIndex.count(txd.id) > 0) 
       {
@@ -409,12 +405,6 @@ namespace CryptoNote {
 
       TransactionCheckInfo checkInfo(txd);
       bool ready = is_transaction_ready_to_go(txd.tx, checkInfo);
-
-      // update item state
-      m_fee_index.modify(i, [&checkInfo](TransactionCheckInfo& item) 
-      {
-        item = checkInfo;
-      });
 
       if (ready && blockTemplate.addTransaction(txd.id, txd.tx)) 
       {
@@ -422,48 +412,6 @@ namespace CryptoNote {
         fee += txd.fee;
       }
     }
-  }
-
-  if (height < CryptoNote::parameters::DEPOSIT_HEIGHT_V3) 
-  {
-    /* Process other transactions starting from the oldest */
-    for (auto i = m_fee_index.begin(); i != m_fee_index.end(); ++i) 
-    {
-      const auto& txd = *i;
-
-      if (m_ttlIndex.count(txd.id) > 0) 
-      {
-        continue;
-      }
-
-      size_t blockSizeLimit = (txd.fee == 0) ? median_size : max_total_size;
-      if (blockSizeLimit < total_size + txd.blobSize) 
-      {
-        continue;
-      }
-
-      TransactionCheckInfo checkInfo(txd);
-      bool ready = is_transaction_ready_to_go(txd.tx, checkInfo);
-
-      // update item state
-      m_fee_index.modify(i, [&checkInfo](TransactionCheckInfo& item) 
-      {
-        item = checkInfo;
-      });
-
-      if (ready && blockTemplate.addTransaction(txd.id, txd.tx)) 
-      {
-        total_size += txd.blobSize;
-        fee += txd.fee;
-      }
-    }    
-    
-  }
-  
-
-
-
-   
 
     bl.transactionHashes = blockTemplate.getTransactions();
     return true;
