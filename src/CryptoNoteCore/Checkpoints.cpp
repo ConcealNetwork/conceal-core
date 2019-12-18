d416709f74b5a326c0b9df6f143460bf00491c77
katz <cryptokatz1@protonmail.com>
2019-12-18 15:17:45 +0530

    Update Checkpoints.cpp

--- src/CryptoNoteCore/Checkpoints.cpp
+++ src/CryptoNoteCore/Checkpoints.cpp
@@ -1,7 +1,7 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-// Copyright (c) 2016-2018, The Karbo developers
+// Copyright (c) 2016-2019, The Karbo developers
 
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
