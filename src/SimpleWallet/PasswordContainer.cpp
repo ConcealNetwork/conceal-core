617c88d74a9e398858e835ed18d5652eca2b7187
Lithy <49086787+LithyRiolu@users.noreply.github.com>
2020-04-24 21:31:34 +0100

    Revert copyrights

--- src/SimpleWallet/PasswordContainer.cpp
+++ src/SimpleWallet/PasswordContainer.cpp
@@ -1,8 +1,6 @@
 // Copyright (c) 2011-2017 The Cryptonote developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
 // Copyright (c) 2018-2019 Conceal Network & Conceal Devs
-// Copyright (c) 2019-2020 The Lithe Project Development Team
-
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
