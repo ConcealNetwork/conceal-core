d2d472345d9411dabb1d9bf42dbb55290b2a49c1
Travis Winsor <travis_winsor@hotmail.com>
2020-06-27 22:20:19 +1000

    fix defines for non arm

--- src/crypto/soft_aes.hpp
+++ src/crypto/soft_aes.hpp
@@ -9,7 +9,7 @@
 
 #pragma once
 
-#if defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#if defined(__GNUC__) && (!defined(__ARM_FEATURE_SIMD32) || !defined(__ARM_NEON))
 	#include <x86intrin.h>
 #elif defined(__ARM_FEATURE_SIMD32) || defined(__ARM_NEON)
 	#include "sse2neon.h"
