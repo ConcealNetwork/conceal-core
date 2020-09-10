762bff8579cba0c1c58a1d31eba5951d699b888e
katz <cryptokatz1@protonmail.com>
2020-09-10 14:56:26 +0530

    Change to boost::placeholders

--- src/P2p/NetNode.cpp
+++ src/P2p/NetNode.cpp
@@ -221,7 +221,12 @@ namespace CryptoNote
     s(m_config.m_peer_id, "peer_id");
   }
 
-#define INVOKE_HANDLER(CMD, Handler) case CMD::ID: { ret = invokeAdaptor<CMD>(cmd.buf, out, ctx,  boost::bind(Handler, this, _1, _2, _3, _4)); break; }
+#define INVOKE_HANDLER(CMD, Handler)                                                                                                                                             \
+  case CMD::ID:                                                                                                                                                                  \
+  {                                                                                                                                                                              \
+    ret = invokeAdaptor<CMD>(cmd.buf, out, ctx, boost::bind(Handler, this, boost::placeholders::_1, boost::placeholders::_2, boost::placeholders::_3, boost::placeholders::_4)); \
+    break;                                                                                                                                                                       \
+  }
 
   int NodeServer::handleCommand(const LevinProtocol::Command& cmd, BinaryArray& out, P2pConnectionContext& ctx, bool& handled) {
     int ret = 0;
