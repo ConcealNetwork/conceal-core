e3436c6750f0f0268f952b36dde118ce4ece2c22
AxVultis <axvultis@gmail.com>
2020-12-28 14:11:41 +0100

    Fix hardware aes detection

--- src/crypto/cryptonight.cpp
+++ src/crypto/cryptonight.cpp
@@ -11,23 +11,23 @@ namespace Crypto {
 
 void cn_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash) {
 	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
 		cryptonight_hash<false, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
+	else
+		cryptonight_hash<true, CRYPTONIGHT>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
 void cn_fast_slow_hash_v1(cn_context &context, const void *data, size_t length, Hash &hash) {
 	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
 		cryptonight_hash<false, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
+	else
+		cryptonight_hash<true, CRYPTONIGHT_FAST_V8>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
 void cn_conceal_slow_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash) {
 	if(hw_check_aes())
-		cryptonight_hash<true, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
-	else
 		cryptonight_hash<false, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
+	else
+		cryptonight_hash<true, CRYPTONIGHT_CONCEAL>(data, length, reinterpret_cast<char *>(&hash), context);
 }
 
 void cn_gpu_hash_v0(cn_context &context, const void *data, size_t length, Hash &hash) {
