6e751283e53286e1909486498022c131fe91b139
Jay Taylor <92698655+cartoon-face@users.noreply.github.com>
2022-01-11 16:11:48 +0000

    Replace with auto

--- src/CryptoNoteCore/BlockIndex.cpp
+++ src/CryptoNoteCore/BlockIndex.cpp
@@ -51,7 +51,7 @@ namespace cn {
     std::vector<crypto::Hash> result;
     if (getBlockHeight(startBlockId, startBlockHeight))
     {
-      size_t sparseChainEnd = static_cast<size_t>(startBlockHeight + 1);
+      auto sparseChainEnd = static_cast<size_t>(startBlockHeight + 1);
       for (size_t i = 1; i <= sparseChainEnd; i *= 2)
       {
         result.emplace_back(m_container[sparseChainEnd - i]);
