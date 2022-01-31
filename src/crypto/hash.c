9835746152ad36a90258a4c445c10081d36fa321
Øx <35565671+krypt0x@users.noreply.github.com>
2022-01-31 21:32:00 +0000

    Copyright 2022
    Updating Copyright for 2022

--- src/crypto/hash.c
+++ src/crypto/hash.c
@@ -1,6 +1,7 @@
 // Copyright (c) 2012-2016, The CryptoNote developers, The Bytecoin developers
 // Copyright (c) 2017-2018 The Circle Foundation & Conceal Devs
-// Copyright (c) 2018-2021 Conceal Network & Conceal Devs
+// Copyright (c) 2018-2022 Conceal Network & Conceal Devs
+//
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
