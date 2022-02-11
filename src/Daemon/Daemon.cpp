b9d0c4ecaceea63ecbd924d69b839a39d046cb11
Aiwe <maksymivv@gmail.com>
2022-02-11 14:23:20 +0200

    Init checkpoints early otherwise core and blockchain won't see them and will endup in full pow validaion during sync

--- src/Daemon/Daemon.cpp
+++ src/Daemon/Daemon.cpp
@@ -16,6 +16,8 @@
 #include "Common/SignalHandler.h"
 #include "Common/PathTools.h"
 #include "crypto/hash.h"
+#include "CryptoNoteConfig.h"
+#include "CryptoNoteCore/Checkpoints.h"
 #include "CryptoNoteCore/Core.h"
 #include "CryptoNoteCore/CoreConfig.h"
 #include "CryptoNoteCore/CryptoNoteTools.h"
@@ -277,6 +279,17 @@ int main(int argc, char* argv[])
 
     cn::Currency currency = currencyBuilder.currency();
     cn::core ccore(currency, nullptr, logManager, vm["enable-blockchain-indexes"].as<bool>(), vm["enable-autosave"].as<bool>());
+
+    cn::Checkpoints checkpoints(logManager);
+    for (const auto& cp : cn::CHECKPOINTS) {
+      checkpoints.add_checkpoint(cp.height, cp.blockId);
+    }
+
+    checkpoints.load_checkpoints_from_dns();
+
+    if (!testnet_mode) {
+      ccore.set_checkpoints(std::move(checkpoints));
+    }
 
     CoreConfig coreConfig;
     coreConfig.init(vm);
