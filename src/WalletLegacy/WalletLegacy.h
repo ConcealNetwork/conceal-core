a637768756d7da3bfeaf0e60ddc9bf6d6ba534bc
Jay Taylor <jaytaylordev@gmail.com>
2022-02-19 21:05:18 +0000

    Withdraw deposit without vector

--- src/WalletLegacy/WalletLegacy.h
+++ src/WalletLegacy/WalletLegacy.h
@@ -102,6 +102,7 @@ public:
   virtual std::list<TransactionOutputInformation> selectFusionTransfersToSend(uint64_t threshold, size_t minInputCount, size_t maxInputCount);
   virtual TransactionId sendFusionTransaction(const std::list<TransactionOutputInformation>& fusionInputs, uint64_t fee, const std::string& extra = "", uint64_t mixIn = 0, uint64_t unlockTimestamp = 0);
   virtual TransactionId deposit(uint32_t term, uint64_t amount, uint64_t fee, uint64_t mixIn = parameters::MINIMUM_MIXIN) override;
+  virtual TransactionId withdrawDeposit(const DepositId& depositId, uint64_t fee) override;
   virtual TransactionId withdrawDeposits(const std::vector<DepositId>& depositIds, uint64_t fee) override;
   virtual std::error_code cancelTransaction(size_t transactionId) override;
 
