ef629aa796c82a3539a86733f687cb3ffb638a6b
AxVultis <axvultis@protonmail.com>
2022-02-26 16:05:28 +0100

    Update miniupnpc to version 2.2.3

--- external/miniupnpc/testminiwget.sh
+++ external/miniupnpc/testminiwget.sh
@@ -1,26 +1,44 @@
 #!/bin/sh
-# $Id: testminiwget.sh,v 1.10 2013/11/13 15:08:08 nanard Exp $
+# $Id: testminiwget.sh,v 1.18 2020/05/29 21:14:22 nanard Exp $
+# vim: tabstop=4 shiftwidth=4 noexpandtab
 # project miniupnp : http://miniupnp.free.fr/
-# (c) 2011-2012 Thomas Bernard
+# (c) 2011-2019 Thomas Bernard
 #
 # test program for miniwget.c
 # is usually invoked by "make check"
 #
 # This test program :
 #  1 - launches a local HTTP server (minihttptestserver)
-#  2 - uses testminiwget to retreive data from this server
+#  2 - uses testminiwget to retrieve data from this server
 #  3 - compares served and received data
 #  4 - kills the local HTTP server and exits
 #
 # The script was tested and works with ksh, bash
 # it should now also run with dash
 
-TMPD=`mktemp -d miniwgetXXXXXXXXXX`
+TMPD=`mktemp -d -t miniwgetXXXXXXXXXX`
+TESTSERVER=./build/minihttptestserver
+TESTMINIWGET=./build/testminiwget
 HTTPSERVEROUT="${TMPD}/httpserverout"
 EXPECTEDFILE="${TMPD}/expectedfile"
 DOWNLOADEDFILE="${TMPD}/downloadedfile"
 PORT=
 RET=0
+IPCONFIG=$(which ifconfig)
+IP=$(which ip)
+if [ "$IP" ] ; then
+	if ! $IP addr | grep inet6 ; then
+		HAVE_IPV6=no
+	fi
+else
+	if [ -z "$IPCONFIG" ] ; then
+		IPCONFIG="/sbin/ifconfig"
+	fi
+
+	if ! $IPCONFIG -a | grep inet6 ; then
+		HAVE_IPV6=no
+	fi
+fi
 
 case "$HAVE_IPV6" in
     n|no|0)
@@ -34,24 +52,35 @@ case "$HAVE_IPV6" in
 
 esac
 
-#make minihttptestserver
-#make testminiwget
+if [ ! -x "$TESTSERVER" ] || [ ! -x "$TESTMINIWGET" ] ; then
+	echo "Please build $TESTSERVER and $TESTMINIWGET"
+	#make minihttptestserver
+	#make testminiwget
+	exit 1
+fi
 
 # launching the test HTTP server
-./minihttptestserver $SERVERARGS -e $EXPECTEDFILE > $HTTPSERVEROUT &
+$TESTSERVER $SERVERARGS -e $EXPECTEDFILE > $HTTPSERVEROUT &
 SERVERPID=$!
 while [ -z "$PORT" ]; do
 	sleep 1
 	PORT=`cat $HTTPSERVEROUT | sed 's/Listening on port \([0-9]*\)/\1/' `
 done
+if [ "$PORT" = "*** ERROR ***" ]; then
+	echo "HTTP test server error"
+	echo "Network config :"
+	$IPCONFIG -a
+	exit 2
+fi
 echo "Test HTTP server is listening on $PORT"
 
 URL1="http://$ADDR:$PORT/index.html"
 URL2="http://$ADDR:$PORT/chunked"
 URL3="http://$ADDR:$PORT/addcrap"
+URL4="http://$ADDR:$PORT/malformed"
 
 echo "standard test ..."
-./testminiwget $URL1 "${DOWNLOADEDFILE}.1"
+$TESTMINIWGET $URL1 "${DOWNLOADEDFILE}.1"
 if cmp $EXPECTEDFILE "${DOWNLOADEDFILE}.1" ; then
 	echo "ok"
 else
@@ -60,7 +89,7 @@ else
 fi
 
 echo "chunked transfert encoding test ..."
-./testminiwget $URL2 "${DOWNLOADEDFILE}.2"
+$TESTMINIWGET $URL2 "${DOWNLOADEDFILE}.2"
 if cmp $EXPECTEDFILE "${DOWNLOADEDFILE}.2" ; then
 	echo "ok"
 else
@@ -69,13 +98,16 @@ else
 fi
 
 echo "response too long test ..."
-./testminiwget $URL3 "${DOWNLOADEDFILE}.3"
+$TESTMINIWGET $URL3 "${DOWNLOADEDFILE}.3"
 if cmp $EXPECTEDFILE "${DOWNLOADEDFILE}.3" ; then
 	echo "ok"
 else
 	echo "response too long test FAILED"
 	RET=1
 fi
+
+echo "malformed response test ..."
+$TESTMINIWGET $URL4 "${DOWNLOADEDFILE}.4"
 
 # kill the test HTTP server
 kill $SERVERPID
