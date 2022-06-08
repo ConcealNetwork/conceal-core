00cee590922308f4e6b09fffdb7a83684db4170b
AxVultis <axvultis@protonmail.com>
2022-06-08 19:53:53 +0200

    Use correct height and unlock height

--- src/SimpleWallet/DepositHelper.cpp
+++ src/SimpleWallet/DepositHelper.cpp
@@ -9,22 +9,22 @@
 
 namespace cn
 {
-  uint32_t deposit_helper::deposit_term(cn::Deposit deposit)
+  uint32_t deposit_helper::deposit_term(const cn::Deposit &deposit) const
   {
     return deposit.term;
   }
 
-  std::string deposit_helper::deposit_amount(cn::Deposit deposit, const Currency& currency)
+  std::string deposit_helper::deposit_amount(const cn::Deposit &deposit, const Currency &currency) const
   {
     return currency.formatAmount(deposit.amount);
   }
 
-  std::string deposit_helper::deposit_interest(cn::Deposit deposit, const Currency& currency)
+  std::string deposit_helper::deposit_interest(const cn::Deposit &deposit, const Currency &currency) const
   {
     return currency.formatAmount(deposit.interest);
   }
 
-  std::string deposit_helper::deposit_status(cn::Deposit deposit)
+  std::string deposit_helper::deposit_status(const cn::Deposit &deposit) const
   {
     std::string status_str = "";
 
@@ -38,48 +38,71 @@ namespace cn
     return status_str;
   }
 
-  size_t deposit_helper::deposit_creating_tx_id(cn::Deposit deposit)
+  size_t deposit_helper::deposit_creating_tx_id(const cn::Deposit &deposit) const
   {
     return deposit.creatingTransactionId;
   }
 
-  size_t deposit_helper::deposit_spending_tx_id(cn::Deposit deposit)
+  size_t deposit_helper::deposit_spending_tx_id(const cn::Deposit &deposit) const
   {
     return deposit.spendingTransactionId;
   }
 
-  uint64_t deposit_helper::deposit_unlock_height(cn::Deposit deposit)
-  {
-    return deposit.unlockHeight;
-  }
-
-  uint64_t deposit_helper::deposit_height(cn::WalletLegacyTransaction txInfo)
-  {
-    return txInfo.blockHeight;
-  }
-
-  std::string deposit_helper::get_deposit_info(cn::Deposit deposit, cn::DepositId did, const Currency& currency, cn::WalletLegacyTransaction txInfo)
+  std::string deposit_helper::deposit_unlock_height(const cn::Deposit &deposit, const cn::WalletLegacyTransaction &txInfo) const
   {
     std::string unlock_str = "";
-    uint64_t actual_unlock_height = deposit_height(txInfo) + deposit_term(deposit);
 
-    bool bad_unlock = actual_unlock_height > cn::parameters::CRYPTONOTE_MAX_BLOCK_NUMBER;
-    if (bad_unlock)
+    if (txInfo.blockHeight > cn::parameters::CRYPTONOTE_MAX_BLOCK_NUMBER)
+    {
       unlock_str = "Please wait.";
+    }
     else
-      unlock_str = std::to_string(actual_unlock_height);
+    {
+      unlock_str = std::to_string(txInfo.blockHeight + deposit_term(deposit));
+    }
 
     bool bad_unlock2 = unlock_str == "0";
     if (bad_unlock2)
+    {
       unlock_str = "ERROR";
+    }
 
+    return unlock_str;
+  }
+
+  std::string deposit_helper::deposit_height(const cn::WalletLegacyTransaction &txInfo) const
+  {
+    std::string height_str = "";
+    uint64_t deposit_height = txInfo.blockHeight;
+
+    bool bad_unlock = deposit_height > cn::parameters::CRYPTONOTE_MAX_BLOCK_NUMBER;
+    if (bad_unlock)
+    {
+      height_str = "Please wait.";
+    }
+    else
+    {
+      height_str = std::to_string(deposit_height);
+    }
+
+    bool bad_unlock2 = height_str == "0";
+    if (bad_unlock2)
+    {
+      height_str = "ERROR";
+    }
+
+    return height_str;
+  }
+
+  std::string deposit_helper::get_deposit_info(const cn::Deposit &deposit, cn::DepositId did, const Currency &currency, const cn::WalletLegacyTransaction &txInfo) const
+  {
     std::stringstream full_info;
 
     full_info << std::left <<
       std::setw(8)  << common::makeCenteredString(8, std::to_string(did)) << " | " <<
       std::setw(20) << common::makeCenteredString(20, deposit_amount(deposit, currency)) << " | " <<
       std::setw(20) << common::makeCenteredString(20, deposit_interest(deposit, currency)) << " | " <<
-      std::setw(16) << common::makeCenteredString(16, unlock_str) << " | " <<
+      std::setw(16) << common::makeCenteredString(16, deposit_unlock_height(deposit, txInfo)) << " | " <<
       std::setw(10) << common::makeCenteredString(10, deposit_status(deposit));
     
     std::string as_str = full_info.str();
@@ -87,30 +110,16 @@ namespace cn
     return as_str;
   }
 
-  std::string deposit_helper::get_full_deposit_info(cn::Deposit deposit, cn::DepositId did, const Currency& currency, cn::WalletLegacyTransaction txInfo)
+  std::string deposit_helper::get_full_deposit_info(const cn::Deposit &deposit, cn::DepositId did, const Currency &currency, const cn::WalletLegacyTransaction &txInfo) const
   {
-    std::string unlock_str = "";
-    uint64_t actual_unlock_height = deposit_height(txInfo) + deposit_term(deposit);
-
-    bool bad_unlock = actual_unlock_height > cn::parameters::CRYPTONOTE_MAX_BLOCK_NUMBER;
-    if (bad_unlock)
-      unlock_str = "Please wait.";
-    else
-      unlock_str = std::to_string(actual_unlock_height);
-
-    bool bad_unlock2 = unlock_str == "0";
-    if (bad_unlock2)
-      unlock_str = "ERROR";
-
     std::stringstream full_info;
 
-    full_info <<
-      "ID:            "  << std::to_string(did) << "\n" <<
-      "Amount:        " << deposit_amount(deposit, currency) << "\n" <<
-      "Interest:      " << deposit_interest(deposit, currency) << "\n" <<
-      "Height:        " << deposit_height(txInfo) << "\n" <<
-      "Unlock Height: " << unlock_str << "\n" <<
-      "Status:        " << deposit_status(deposit) << "\n";
+    full_info << "ID:            " << std::to_string(did) << "\n"
+              << "Amount:        " << deposit_amount(deposit, currency) << "\n"
+              << "Interest:      " << deposit_interest(deposit, currency) << "\n"
+              << "Height:        " << deposit_height(txInfo) << "\n"
+              << "Unlock Height: " << deposit_unlock_height(deposit, txInfo) << "\n"
+              << "Status:        " << deposit_status(deposit) << "\n";
 
     std::string as_str = full_info.str();
 
--- src/SimpleWallet/DepositHelper.h
+++ src/SimpleWallet/DepositHelper.h
@@ -1,6 +1,5 @@
 
 
-
 #include <string>
 
 #include "IWalletLegacy.h"
@@ -16,51 +15,51 @@ namespace cn
      * @param deposit - obtain information from deposit
      * @return - returns deposit term, should be multiple of 21900
     **/
-    uint32_t deposit_term(cn::Deposit deposit);
+    uint32_t deposit_term(const cn::Deposit &deposit) const;
 
     /**
      * @param deposit - obtain information from deposit
      * @param currency - format output amount
      * @return - returns deposit amount as string
     **/
-    std::string deposit_amount(cn::Deposit deposit, const Currency& currency);
+    std::string deposit_amount(const cn::Deposit &deposit, const Currency &currency) const;
 
     /**
      * @param deposit  - obtain information from deposit
      * @param currency - format output amount
      * @return - returns deposit interest based of amount and term as string
     **/
-    std::string deposit_interest(cn::Deposit deposit, const Currency& currency);
+    std::string deposit_interest(const cn::Deposit &deposit, const Currency &currency) const;
 
     /**
      * @param deposit - obtain information from deposit
      * @return - "Locked", "Unlocked" or "Spent" depending on state of deposit
     **/
-    std::string deposit_status(cn::Deposit deposit);
+    std::string deposit_status(const cn::Deposit &deposit) const;
 
     /**
      * @param deposit - obtain information from deposit
      * @return - returns deposit creatingTransactionId 
     **/
-    size_t deposit_creating_tx_id(cn::Deposit deposit);
+    size_t deposit_creating_tx_id(const cn::Deposit &deposit) const;
 
     /**
      * @param deposit - obtain information from deposit
      * @return - returns deposit spendingTransactionId
     **/
-    size_t deposit_spending_tx_id(cn::Deposit deposit);
+    size_t deposit_spending_tx_id(const cn::Deposit &deposit) const;
 
     /**
      * @param deposit - obtain information from deposit
      * @return - returns unlock height from height deposited + term
     **/
-    uint64_t deposit_unlock_height(cn::Deposit deposit);
+    std::string deposit_unlock_height(const cn::Deposit &deposit, const cn::WalletLegacyTransaction &txInfo) const;
 
     /**
      * @param txInfo - obtain height from transaction
      * @return - returns deposit height
     **/
-    uint64_t deposit_height(cn::WalletLegacyTransaction txInfo);
+    std::string deposit_height(const cn::WalletLegacyTransaction &txInfo) const;
 
     /**
      * @param deposit - obtain information from deposit
@@ -69,7 +68,7 @@ namespace cn
      * @param txInfo - obtain height from transaction
      * @return - returns deposit info string in single row style
     **/
-    std::string get_deposit_info(cn::Deposit deposit, cn::DepositId did, const Currency& currency, cn::WalletLegacyTransaction txInfo);
+    std::string get_deposit_info(const cn::Deposit &deposit, cn::DepositId did, const Currency &currency, const cn::WalletLegacyTransaction &txInfo) const;
 
     /**
      * @param deposit - obtain information from deposit
@@ -78,6 +77,6 @@ namespace cn
      * @param txInfo - obtain height from transaction
      * @return - returns full deposit info string
     **/
-    std::string get_full_deposit_info(cn::Deposit deposit, cn::DepositId did, const Currency& currency, cn::WalletLegacyTransaction txInfo);
+    std::string get_full_deposit_info(const cn::Deposit &deposit, cn::DepositId did, const Currency &currency, const cn::WalletLegacyTransaction &txInfo) const;
   };
 }
