ac0cc4922d529aadbf3ff4c5cba28dc1f5ed93eb
cartoon-face <jaytaylordev@gmail.com>
2022-07-29 02:02:03 +0100

    Add COPYRIGHT header to new files

--- src/ConcealWallet/ClientHelper.cpp
+++ src/ConcealWallet/ClientHelper.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2018-2022 Conceal Network & Conceal Devs
+//
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #include <ctime>
 #include <future>
