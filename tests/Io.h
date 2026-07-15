413b9f5467264e82d09a8ed33fe0ab1b2279c4cc
acktarius <acktarius@protonmail.com>
2026-07-14 23:36:10 -0400

    fix(tests): io.h undefine hexdecode on Windows. Windows #include <io.h> would otherwise resolve to tests/Io.h (case-insensitive) and redefine its helpers.

--- tests/Io.h
+++ tests/Io.h
@@ -3,6 +3,8 @@
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
+#pragma once
+
 #include <cstddef>
 #include <ios>
 #include <iostream>
